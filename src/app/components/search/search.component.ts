a4a900a5174764ca389d49b84cac3f067894ad40
Ali Padron <alijesus29@gmail.com>
2020-01-27 15:32:16 -0400

    Basic search results.

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
+  selector: "app-search",
+  templateUrl: "./search.component.html",
   styles: []
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  artistas: any[] = [];
 
-  ngOnInit() {
+  constructor(private spotifyService: SpotifyService) {}
+
+  ngOnInit() {}
+
+  buscar(termino: string) {
+    this.spotifyService.getArtista(termino).subscribe((data: any) => {
+      this.artistas = data.artists.items;
+    });
   }
-
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root"
@@ -16,5 +16,18 @@ export class SpotifyService {
     return this.http.get("https://api.spotify.com/v1/browse/new-releases", {
       headers
     });
+  }
+
+  getArtista(termino: string) {
+    const headers = new HttpHeaders({
+      Authorization:
+        "Bearer BQA9ssAXcP-nyzT0lptDG4N7AROb2MMf8MN6l0qjOvmGOr6P3xFC2A61-EQ1tkcGmnwAquVxNp8FTOkNrmg"
+    });
+    return this.http.get(
+      `https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`,
+      {
+        headers
+      }
+    );
   }
 }
