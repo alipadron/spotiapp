30efe961def0511d081bbca98a05d8f8b02d1aee
Ali Padron <alijesus29@gmail.com>
2020-01-31 18:42:24 -0400

    Show error message in home component.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,12 +9,27 @@ import { SpotifyService } from "src/app/services/spotify.service";
 export class HomeComponent implements OnInit {
   nuevasCanciones: any[] = [];
   loading = true;
+  error = false;
+  mensajeError: string;
   constructor(private spotifyService: SpotifyService) {}
 
   ngOnInit() {
-    this.spotifyService.getNewReleases().subscribe((data: any[]) => {
-      this.nuevasCanciones = data;
-      this.loading = false;
-    });
+    this.spotifyService.getNewReleases().subscribe(
+      (data: any[]) => {
+        this.nuevasCanciones = data;
+        this.loading = false;
+      },
+      errorServicio => {
+        console.log(errorServicio);
+        this.loading = false;
+        this.error = true;
+        this.mensajeError =
+          (errorServicio.error &&
+            errorServicio.error.error &&
+            errorServicio.error.error.message) ||
+          (errorServicio.error && errorServicio.error.message) ||
+          errorServicio.message;
+      }
+    );
   }
 }
